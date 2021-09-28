c87271366df79439edb6afb6daa88e587ab8b433
yurykurylovich <yury.kurylovich@gmail.com>
2021-09-28 16:24:56 +0300

    Update footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,14 @@
 import React from "react";
-import FooterIcon from "../Icons/FooterIcon.js";
+import FooterIcon from "../Icons/FooterIcon";
 import s from "./Footer.module.scss";
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <div className={s.footer}>
-        <span className={s.footerLabel}>2021 &copy; Flatlogic. Hand-crafted & Made with</span>
-        <FooterIcon />
-      </div>
-    )
-  }
+const Footer = () => {
+  return (
+    <div className={s.footer}>
+      <span className={s.footerLabel}>2021 &copy; Flatlogic. Hand-crafted & Made with</span>
+      <FooterIcon />
+    </div>
+  )
 }
 
 export default Footer;
